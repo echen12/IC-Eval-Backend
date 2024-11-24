ae5509c48d720f432fb4dc7598df71352a49f6ba
echen12 <chen.edmond13@gmail.com>
2024-11-24 14:02:23 -0800

    Created a minimal api to serve json form config contents.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Allow cors for dev purposes
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()  // This will allow requests from any origin
+            .AllowAnyMethod()    // Allow any HTTP method (GET, POST, etc.)
+            .AllowAnyHeader();   // Allow any HTTP header
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseCors();
+app.UseRouting();  
+app.MapGet("/data", async () =>
+{
+    var jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "StaticFiles", "data.json");
+    var jsonData = await File.ReadAllTextAsync(jsonFilePath);
+    return Results.Text(jsonData, "application/json");
+});
+
 
 app.Run();
